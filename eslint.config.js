1bddab03067c76b117b54c5e8492b5f6a69c04fc
G.mota <virajj97112@gmail.com>
2025-07-23 09:05:12 +0530

    setup with ci-pretter-lint-husky

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { default as react, default as reactPlugin } from "eslint-plugin-react";
+import globals from "globals";
+import prettier from "prettier";
+
+export default [
+  {
+    ignores: ["dist/**/*"],
+  },
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    ignores: ["dist/**/*"],
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      version: "detect",
+    },
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      react,
+      prettier,
+    },
+    rules: {
+      "react/no-unknown-property": "warn",
+    },
+  },
+  eslintConfigPrettier,
+];
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="center-align">
+      <div className="animate text-2xl ">
+        WIP : Portfolio VN
+      </div>
+    </div>
+  )
+}
+
+export default App
