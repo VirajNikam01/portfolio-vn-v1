ad42a646307ad15fae5b6fcf823c7099214dd049
virajnikam01 <virajnikam01@gmail.com>
2025-07-31 22:56:45 +0530

    feat : added dev test

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const App = () => {
       <div className="orange animate">
         <h1 className="text-white">Portfolio VN</h1>
         <h2 className="text-white">Version 1.0</h2>
+        <h3 className="text-white">Development</h3>
         {t("welcome")}
       </div>
     </div>
