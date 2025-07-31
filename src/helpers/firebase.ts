f8ca3dceef4ee6a3b80ec012ca28f14adbf50616
virajnikam01 <virajnikam01@gmail.com>
2025-07-31 23:55:58 +0530

    feat : used best practices

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCHSK8v_S9NV_iD94F9w57VclLSXMRi6eE",
-    authDomain: "portfolio-vn-v1.firebaseapp.com",
-    projectId: "portfolio-vn-v1",
-    storageBucket: "portfolio-vn-v1.firebasestorage.app",
-    messagingSenderId: "877799270447",
-    appId: "1:877799270447:web:c2120b73f0ac88cd297eda",
-    measurementId: "G-PTJNYV7R2D"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
